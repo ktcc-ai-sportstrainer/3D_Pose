256907eba5197a9d09836f64393324d7ce8f41bb
DeL-TaiseiOzaki <symonds6457@outlook.jp>
2024-12-25 18:24:54 +0900

    first commit

---
+++ AlphaPose/alphapose/utils/roi_align/__init__.py
@@ -0,0 +1,4 @@
+# from AlphaPose.alphapose.utils.roi_align.roi_align import roi_align, RoIAlign
+
+# __all__ = ['roi_align', 'RoIAlign']
+# AlphaPose/alphapose/utils/roi_align/roi_align.py
---
+++ AlphaPose/alphapose/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Tue Dec 17 11:52:10 2024
+
+__version__ = '0.5.0+cc2d89b'
+short_version = '0.5.0'
